3376e4d3ad38d2d644f8703d62fbb86189919c20
AnamikaAg <ana057@ucsd.edu>
2020-06-13 17:21:34 -0700

    Update and rename Readme to Readme.m

---
+++ Final_code/Readme.m
@@ -0,0 +1,2 @@
+# Please go through Readme.txt for information on how to use individual preprocessing, u-net and postprocessing modules. 
+# For information on how to produce image-mask pairs for training using ImageJ, please refer to imageJmasks.md
